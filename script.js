24bad44c86802a05af8e2bb9cc1dd4f7d9cb6648
mlaane <goobric@gmail.com>
2023-12-26 17:43:26 +0000

    JS functionality for project

---
+++ script.js
@@ -0,0 +1,4 @@
+// fCC JS Music Player project
+const playlistSongs = document.getElementById('playlist-songs');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
